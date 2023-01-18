468ac47c05172ab5031db757571e032db2f39fb4
Luciano Schmarsow <78238612+clarkos@users.noreply.github.com>
2023-01-18 21:25:20 +0000

    allowing changes persist on DB

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const { Recipe, Diet } = require("./src/db.js");
 
 // Syncing all the models at once.
 conn
-  .sync({ force: true })
+  .sync()
   .then(() => {
     server.listen(3001, () => {
       console.log("Server is listening at 3001"); // eslint-disable-line no-console
